565558ccef3e351a0cbb06b315bd075c7ab2b3a0
iSlooZ <mic.espinoza@duocuc.cl>
2023-10-25 19:17:45 -0300

    Arreglo de botón del mapa

--- app/components/maps/ButtonMaps.jsx
+++ app/components/maps/ButtonMaps.jsx
@@ -1,13 +1,25 @@
-export default function ButtonMaps({latitude, longitude}){
-  
-  const googleMapsURL = `https://www.google.com/maps?q=${latitude},${longitude}`;
+export default function ButtonMaps({ latitude, longitude }) {
+  // URL de Google Maps con origen y destino especificados.
+  const googleMapsURL = `https://www.google.com/maps/dir/?api=1&origin=Mi+Ubicacion&destination=${latitude},${longitude}`;
+
+  const isAndroid = /Android/i.test(navigator.userAgent);
+  const isWindows = /Windows/i.test(navigator.userAgent);
+
+  const openGoogleMaps = () => {
+    if (isAndroid) {
+      // En dispositivos Android, utiliza el esquema "geo:" para abrir la aplicación de mapas.
+      window.open(`geo:${latitude},${longitude}?q=${latitude},${longitude}`, '_blank');
+    } else if (isWindows) {
+      // En sistemas Windows, abre la URL de Google Maps con origen y destino especificados.
+      window.open(googleMapsURL, '_blank');
+    }
+  };
 
   return (
     <div className="mt-[10px]">
-      <a className="p-[10px]  bg-[#318CE7] rounded-3xl text-white" href={googleMapsURL} target="_blank" rel="noopener noreferrer">
-        <button>Ir a Google Maps</button>
-      </a>
+      <button onClick={openGoogleMaps} className="p-[10px]  bg-[#318CE7] rounded-3xl text-white">
+        Ir a Google Maps
+      </button>
     </div>
   );
-
 }
