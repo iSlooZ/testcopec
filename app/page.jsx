967a138c26b79a95409642e9ba477cc7cf42cb9e
iSlooZ <mic.espinoza@duocuc.cl>
2023-10-25 18:12:32 -0300

    ultima modificación

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,14 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import Filter from '@/components/filter/Filter';
-import Image from 'next/image';
 
 export default function Bencina() {
   const [data, setData] = useState([]);
   const [selectedComuna, setSelectedComuna] = useState('');
+  const [selectedGasolina, setSelectedGasolina] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
-    // Realiza la solicitud a la API y establece los datos en el estado
     const fetchData = async () => {
       try {
         const res = await fetch(`https://api.bencinaenlinea.cl/api/busqueda_estacion_filtro`, { cache: 'no-store' });
@@ -19,39 +19,87 @@ export default function Bencina() {
       }
     };
 
-    fetchData(); // Llama a la función para obtener datos cuando el componente se monta
+    fetchData();
   }, []);
+
+  useEffect(() => {
+    const filterData = () => {
+      if (!selectedComuna || !selectedGasolina) {
+        setFilteredData([]);
+        return;
+      }
+  
+      const filtered = data
+        .filter(estacion => estacion.comuna === selectedComuna)
+        .map(estacion => {
+          const filteredCombustibles = estacion.combustibles
+            .filter(combustible => [1, 7, 2].includes(combustible.id) && combustible.id === parseInt(selectedGasolina));
+          
+          if (filteredCombustibles.length > 0) {
+            return { ...estacion, combustibles: filteredCombustibles };
+          }
+          
+          return null;
+        }).filter(Boolean); // Remove stations with no matching gasoline
+  
+      // Sort the data by price from lowest to highest
+      filtered.forEach(estacion => {
+        estacion.combustibles.sort((a, b) => (a.precio || 0) - (b.precio || 0));
+      });
+  
+      // Sort the stations themselves by the price of the first combustible (if available)
+      filtered.sort((a, b) => {
+        const precioA = a.combustibles[0] ? a.combustibles[0].precio || 0 : 0;
+        const precioB = b.combustibles[0] ? b.combustibles[0].precio || 0 : 0;
+        return precioA - precioB;
+      });
+  
+      setFilteredData(filtered);
+    };
+  
+    filterData();
+  }, [selectedComuna, selectedGasolina, data]);
+  
+  
+  
 
   return (
     <div>
-      <Filter data={data} onFilterChange={comuna => setSelectedComuna(comuna)} />
-      {selectedComuna && (
-        <ul className="w-100 bg-white grid gap-4 grid-cols-1 ">
-          {data
-            .filter(estacion => estacion.comuna === selectedComuna)
-            .map(estacion => (
-              <li className="bg-card-color flex flex-col justify-center items-left  rounded-lg p-[20px]" key={estacion.id}>
-                <img className="w-[100px] p-[10px]" src="http://api.bencinaenlinea.cl/storage/logo/1697478048_logo5.jpg" alt="" />
-                <div className="w-100 pl-[10px]">
-                  <h3 className="card-data">Dirección: {estacion.direccion}</h3>
-                  <h4 className="card-data">Región: {estacion.region}</h4>
-                  <h4 className="card-data">Comuna: {estacion.comuna}</h4>
+      <Filter
+        data={data}
+        onFilterChange={(comuna, gasolina) => {
+          setSelectedComuna(comuna);
+          setSelectedGasolina(gasolina);
+        }}
+      />
+      {selectedComuna && selectedGasolina && (
+        <ul className="w-100 bg-white grid gap-4 grid-cols-1">
+          {filteredData.map(estacion => (
+            <li
+              className="bg-card-color flex flex-col justify-center items-left rounded-lg p-[20px]"
+              key={estacion.id}
+            >
+              <img
+                className="w-[100px] p-[10px]"
+                src="http://api.bencinaenlinea.cl/storage/logo/1697478048_logo5.jpg"
+                alt=""
+              />
+              <div className="w-100 pl-[10px]">
+                <h3 className="card-data">Dirección: {estacion.direccion}</h3>
+                <h4 className="card-data">Región: {estacion.region}</h4>
+                <h4 className="card-data">Comuna: {estacion.comuna}</h4>
+              </div>
+              {estacion.combustibles.map(combustible => (
+                <div className="grid grid-cols-3 p-[10px]" key={combustible.id}>
+                  <h1 className="card-data">{combustible.nombre_largo}</h1>
+                  <h2 className="card-data">
+                    Precio: ${Math.floor(combustible.precio).toLocaleString()}
+                  </h2>
+                  <h3 className="card-data">Actualización: {combustible.actualizado}</h3>
                 </div>
-                {estacion.combustibles
-                  .filter(combustible => [1, 7, 2].includes(combustible.id))
-                  .sort((a, b) => {
-                    const order = [1, 7, 2];
-                    return order.indexOf(a.id) - order.indexOf(b.id);
-                  })
-                  .map(combustible => (
-                    <div className="grid grid-cols-3 p-[10px]" key={combustible.id}>
-                      <h1 className="card-data">{combustible.nombre_largo}</h1>
-                      <h2 className="card-data">Precio: ${Math.floor(combustible.precio).toLocaleString()}</h2>
-                      <h3 className="card-data">Actualización: {combustible.actualizado}</h3>
-                    </div>
-                  ))}
-              </li>
-            ))}
+              ))}
+            </li>
+          ))}
         </ul>
       )}
     </div>
--- components/filter/Filter.jsx
+++ components/filter/Filter.jsx
@@ -3,15 +3,23 @@ import React, { useState } from 'react';
 export default function Filter({ data, onFilterChange }) {
   const comunas = [...new Set(data.map(estacion => estacion.comuna))];
 
+
   // Ordena las comunas alfabéticamente por la primera letra
   comunas.sort((a, b) => a.localeCompare(b));
 
   const [selectedComuna, setSelectedComuna] = useState('');
+  const [selectedGasolina, setSelectedGasolina] = useState(''); // Estado para el tipo de gasolina seleccionado
 
   const handleComunaChange = (e) => {
     const comuna = e.target.value;
     setSelectedComuna(comuna);
-    onFilterChange(comuna);
+    onFilterChange(comuna, selectedGasolina); // Llama a la función onFilterChange con comuna y tipo de gasolina
+  };
+
+  const handleGasolinaChange = (e) => {
+    const gasolina = e.target.value;
+    setSelectedGasolina(gasolina);
+    onFilterChange(selectedComuna, gasolina); // Llama a la función onFilterChange con comuna y tipo de gasolina
   };
 
   return (
@@ -28,6 +36,18 @@ export default function Filter({ data, onFilterChange }) {
             {comuna}
           </option>
         ))}
+      </select>
+
+      <label className="block text-sm font-medium text-gray-700 mt-4">Filtrar por Tipo de Gasolina:</label>
+      <select
+        value={selectedGasolina}
+        onChange={handleGasolinaChange}
+        className="mt-1 p-2 rounded border border-gray-300 w-full"
+      >
+        <option value="">Selecciona el tipo de gasolina</option>
+        <option value="1">Gasolina 93</option>
+        <option value="7">Gasolina 95</option>
+        <option value="2">Gasolina 97</option>
       </select>
     </div>
   );
